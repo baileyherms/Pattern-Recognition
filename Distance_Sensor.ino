86fdaf072fc6afa45445423b547a4911ff3a5f34
Bailey Herms <bailey.herms@gmail.com>
2017-02-13 14:27:24 -0800

    Add files via upload

---
+++ Distance_Sensor.ino
@@ -0,0 +1,93 @@
+//Distance Sensor
+//Feature is height
+//Pre-known object of different heights
+//Pattern is see what the object is closest to.
+
+#include <NewPing.h>
+
+#define TRIGGER_PIN 12
+#define ECHO_PIN 11
+#define MAX_DISTANCE 50
+#define K_NEAREST_NEIGHBORS 1
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+struct Features {
+  int height;
+};
+
+Features Feature_Extraction() {
+  Features features;
+  //Get height
+
+  int dist = sonar.ping_cm();
+  
+  features.height = MAX_DISTANCE - dist;
+  
+  return features; 
+}
+
+String Pattern_Recognition(Features features) {
+  //Banana is 3-7cm (3 <= x < 7)
+  //Apple-Orange height is 7-11cm (7 <= x < 11)
+  //Small box is 11-25cm (11 <= x < 25)
+  //Medium box is 25-35cm (25 <= x < 35)
+  //Big box is 35-45cm (35 <= x < 45)
+  //Pre-known object of different heights
+  //Pattern is see what the object is closest to.
+
+  String closest_object;
+
+  Serial.print("Object height: ");
+  Serial.println(features.height);
+
+  //k nearest neighbors
+
+  if(features.height >= 3 && features.height < 7) {
+    closest_object = "Banana";
+  }
+  else if(features.height >= 7 && features.height < 11) {
+    closest_object = "Apple or Orange";
+  }
+  else if(features.height >= 11 && features.height < 25) {
+    closest_object = "Small box";
+  }
+  else if(features.height >= 25 && features.height < 35) {
+    closest_object = "Medium box";
+  }
+  else if(features.height >= 35 && features.height < 45) {
+    closest_object = "Large box";
+  }
+  else {
+    closest_object = "";
+  }
+  return closest_object;
+}
+
+void Actuation(String object) {
+  if(object != "") {
+    Serial.print("\t");
+    Serial.println(object);
+  }
+}
+
+void setup() {
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  //Turns both LEDs off before each reading
+
+  //Should put feature extraction and pattern recognition in one multi-second for loop to
+    //give the box time to go through the sensors.
+  Features feature_states;
+  feature_states = Feature_Extraction();
+
+  String closest_object;
+  closest_object = Pattern_Recognition(feature_states);
+
+  Actuation(closest_object);
+
+  delay(1000);
+}
---
+++ IR_Breakbeam.ino
@@ -0,0 +1,96 @@
+#define LED_SMALL_PIN 14
+#define LED_LARGE_PIN 15
+#define IR_SENSOR_PIN_1 2
+#define IR_SENSOR_PIN_2 3
+
+bool IR_Sensor1;
+bool IR_Sensor2;
+
+struct Features {
+  bool lt_10in;
+};
+
+Features Feature_Extraction() {
+  Features features;
+  
+  IR_Sensor1 = digitalRead(IR_SENSOR_PIN_1);
+  IR_Sensor2 = digitalRead(IR_SENSOR_PIN_2);
+
+  //Do the state machine thing where once the first one is
+    //broken see if the second one is broken at the same time.
+    //Wait a few seconds.
+
+  if(IR_Sensor1 == LOW && IR_Sensor2 == LOW) {
+    features.lt_10in = false;
+  }
+  //If one beam is broken
+  else if(IR_Sensor1 == LOW && IR_Sensor2 == HIGH) {
+    features.lt_10in = true;
+  }
+  else if(IR_Sensor1 == HIGH && IR_Sensor2 == LOW) {
+    features.lt_10in = true;
+  }
+  //If neither beam is broken
+  else {
+    //box_size = "None";
+    features.lt_10in = true;
+  }
+  
+  return features; 
+}
+
+String Pattern_Recognition(Features features) {
+  String box_size;
+
+  //If both beams are broken
+  if(features.lt_10in) {
+    box_size = "Less than 10 inches.";
+  }
+  else {
+    box_size = "Greater than 10 inches.";
+  }
+  return box_size;
+}
+
+void Actuation(String box_size) {
+  Serial.println(box_size);
+  if(box_size == "Less than 10 inches.") {
+    digitalWrite(LED_SMALL_PIN, HIGH);
+  }
+  else if(box_size == "Greater than 10 inches.") {
+    digitalWrite(LED_LARGE_PIN, HIGH);
+  }
+  else if(box_size == "None") {
+    //Both LEDs are off
+  }
+}
+
+void setup() {
+  pinMode(LED_SMALL_PIN, OUTPUT);
+  pinMode(LED_LARGE_PIN, OUTPUT);
+  
+  pinMode(IR_SENSOR_PIN_1, INPUT);
+  digitalWrite(IR_SENSOR_PIN_1, HIGH);
+  pinMode(IR_SENSOR_PIN_2, INPUT);
+  digitalWrite(IR_SENSOR_PIN_2, HIGH);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  //Turns both LEDs off before each reading
+  digitalWrite(LED_SMALL_PIN, LOW);
+  digitalWrite(LED_LARGE_PIN, LOW);
+
+  //Should put feature extraction and pattern recognition in one multi-second for loop to
+    //give the box time to go through the sensors.
+  Features feature_states;
+  feature_states = Feature_Extraction();
+
+  String pattern_size;
+  pattern_size = Pattern_Recognition(feature_states);
+
+  Actuation(pattern_size);
+
+  delay(1000);
+}
