d419bf6122468537c1eb4375cebd439e431d7906
Bailey Herms <bailey.herms@gmail.com>
2017-02-14 00:22:23 -0800

    Add files via upload

--- K_Nearest_Neighbors_Boxes.ino
+++ K_Nearest_Neighbors_Boxes.ino
@@ -2,33 +2,35 @@
 //Feature is height
 //Pre-known object of different heights
 //Pattern is see what the object is closest to.
-
+#include <Arduino.h>
 #include <NewPing.h>
+
+#include "Known_Objects.h"
 
 #define TRIGGER_PIN 12
 #define ECHO_PIN 11
 #define MAX_DISTANCE 50
 //K Nearest Neighbors
-#define K 1
+#define K 3
+
+const size_t NUM_OF_KNOWN = 9;
+const size_t NUM_OF_TYPES = 3;
+String Known_Types[3] = {"Small", "Medium", "Large"};
+Known_Objects Boxes[NUM_OF_KNOWN] = {{"Small", 11},
+                        {"Small", 15},
+                        {"Small", 12},
+                        {"Medium", 27},
+                        {"Medium", 29},
+                        {"Medium", 30},
+                        {"Large", 40},
+                        {"Large", 42},
+                        {"Large", 42}};
+
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 struct Features {
   int height;
-};
-
-//0 = small
-//1 = medium
-//2 = large
-
-//Types of items being measured (rows)
-const int boxes_x = 3;
-//Amount of each item type that is given for k nearest neighbor (columns)
-const int boxes_y = 3;
-int Boxes[boxes_x][boxes_y] = {
-  {11, 15, 12},
-  {27, 29, 30},
-  {40, 42, 42}
 };
 
 Features Feature_Extraction() {
@@ -43,14 +45,9 @@ Features Feature_Extraction() {
 }
 
 String Pattern_Recognition(Features features) {
-  //Banana is 3-7cm (3 <= x < 7)
-  //Apple-Orange height is 7-11cm (7 <= x < 11)
-  //Small box is 11-25cm (11 <= x < 25)
-  //Medium box is 25-35cm (25 <= x < 35)
-  //Big box is 35-45cm (35 <= x < 45)
-  //Pre-known object of different heights
-  //Pattern is see what the object is closest to.
 
+  Known_Objects diff_Boxes[9];
+  
   String closest_object;
 
   Serial.print("Object height: ");
@@ -60,31 +57,79 @@ String Pattern_Recognition(Features features) {
 
   //Checks the min difference between two points
   int min_difference = 100;
-
-  //Position of min_difference
   int pos_min_difference[K];
 
-  for(int i = 0; i < boxes_x; ++i) {
-    for(int j = 0; j < boxes_y; ++j) {
-      if(abs(Boxes[i][j] - features.height) <= min_difference) {
-        min_difference = abs(Boxes[i][j] - features.height);
-        pos_min_difference[0] = i;
+  /*
+  String temp_type;
+  int temp_height;
+  for(int i = 0; i < NUM_OF_KNOWN; ++i) {
+    for(int j = 0; j < NUM_OF_KNOWN - i - 1; ++j) {
+      if(Boxes[j].height > Boxes[j+1].height) {
+        temp_type = Boxes[j].type;
+        temp_height = Boxes[j].height;
+        Boxes[j].type = Boxes[j+1].type;
+        Boxes[j].height = Boxes[j+1].height;
+        Boxes[j+1].type = temp_type;
+        Boxes[j+1].height = temp_height;
+      }
+    }
+  }
+  */
+
+  //MOST NEW CODE STARTS HERE
+  //Find differences between distance and known box sizes and store in difference array
+  for(int i = 0; i < NUM_OF_KNOWN; ++i) {
+    /*
+    if(abs(Boxes[i].height - features.height) <= min_difference) {
+      //min_difference = abs(Boxes[i].height - features.height);
+      //pos_min_difference[0] = i;
+      
+    }
+    */
+    diff_Boxes[i].type = Boxes[i].type;
+    diff_Boxes[i].height = abs(Boxes[i].height - features.height);
+  }
+
+  String temp_type;
+  int temp_height;
+  //Sort the difference array
+  for(int i = 0; i < NUM_OF_KNOWN; ++i) {
+    for(int j = 0; j < NUM_OF_KNOWN - i - 1; ++j) {
+      if(diff_Boxes[j].height > diff_Boxes[j+1].height) {
+        temp_type = diff_Boxes[j].type;
+        temp_height = diff_Boxes[j].height;
+        diff_Boxes[j].type = diff_Boxes[j+1].type;
+        diff_Boxes[j].height = diff_Boxes[j+1].height;
+        diff_Boxes[j+1].type = temp_type;
+        diff_Boxes[j+1].height = temp_height;
       }
     }
   }
 
-  //Serial.println(min_difference);
-  //Serial.println(pos_min_difference);
+  //Create new array holding only K elements
+  Known_Objects k_Nearest_Boxes[K];
+  for(int i = 0; i < K; ++i) {
+    k_Nearest_Boxes[i].type = diff_Boxes[i].type;
+    k_Nearest_Boxes[i].height = diff_Boxes[i].height;
+  }
 
-  if(pos_min_difference[0] == 0) {
-    closest_object = "Small Box";
+  int count = 0;
+  int max_count = 0;
+  String max_type;
+  //Find out which box type occurs the most
+  for(int i = 0; i < NUM_OF_TYPES; ++i) {
+    for(int j = 0; j < NUM_OF_TYPES; ++j) {
+      if(k_Nearest_Boxes[j].type == Known_Types[i]) {
+        count++;
+      }
+    }
+    if(count > max_count) {
+      max_count = count;
+      max_type = Known_Types[i];
+    }
   }
-  else if(pos_min_difference[0] == 1) {
-    closest_object = "Medium Box";
-  }
-  else if(pos_min_difference[0] == 2) {
-    closest_object = "Large Box";
-  }
+
+  closest_object = max_type;
   
   return closest_object;
 }
@@ -96,9 +141,12 @@ void Actuation(String object) {
   }
 }
 
+
+
 void setup() {
   
   Serial.begin(9600);
+  
 }
 
 void loop() {
@@ -106,6 +154,8 @@ void loop() {
 
   //Should put feature extraction and pattern recognition in one multi-second for loop to
     //give the box time to go through the sensors.
+
+    
   Features feature_states;
   feature_states = Feature_Extraction();
 
