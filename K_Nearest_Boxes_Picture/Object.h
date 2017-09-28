17a653bc1417046db3a427ee1d8545f1060f453b
Bailey Herms <bailey.herms@gmail.com>
2017-09-28 02:09:55 -0700

    Add files via upload

---
+++ K_Nearest_Boxes_Picture/Object.h
@@ -0,0 +1,17 @@
+#ifndef __Object__
+#define __Object__
+
+#include "Arduino.h"
+
+typedef struct {
+  String type;
+  float height;
+  float width;
+  float weight;
+  float red;
+  float green;
+  float blue;
+} Object;
+
+
+#endif
