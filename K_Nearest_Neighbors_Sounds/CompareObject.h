2467387a1fac204cfd1a9ac7ce28c16177a5cc6d
Bailey Herms <bailey.herms@gmail.com>
2017-11-29 11:10:18 -0800

    Add files via upload

---
+++ K_Nearest_Neighbors_Sounds/CompareObject.h
@@ -0,0 +1,12 @@
+#ifndef __CompareObject__
+#define __CompareObject__
+
+#include "Arduino.h"
+
+typedef struct {
+  int compfreq[];
+} CompareObject;
+
+
+#endif
+
---
+++ K_Nearest_Neighbors_Sounds/Object.h
@@ -0,0 +1,14 @@
+#ifndef __Object__
+#define __Object__
+
+#include "Arduino.h"
+
+typedef struct {
+  String type;
+  int freq[];
+  int freq_size;
+} Object;
+
+
+#endif
+
